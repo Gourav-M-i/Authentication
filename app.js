7ce817c4b6c9839fc9814561037b8e860655ad19
Gourav Mishra <gm830722@gmail.com>
2022-07-03 20:31:05 +0530

    Hashing password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 
@@ -19,8 +19,6 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
@@ -55,7 +53,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
     User.findOne({email:req.body.username},function(err,foundUser){
         if(foundUser){
-            if(req.body.password===foundUser.password){
+            if(md5(req.body.password)===foundUser.password){
                 res.render("secrets");
             }
             else{
