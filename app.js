645ae565df8e5edf60134cdce1c07913b77308ab
Gourav Mishra <gm830722@gmail.com>
2022-07-03 23:07:28 +0530

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 const app=express();
 
@@ -35,35 +36,35 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }
-        else{
-            console.log(err);
-        }
-    })
-
-});
-
-app.post("/login",function(req,res){
-    User.findOne({email:req.body.username},function(err,foundUser){
-        if(foundUser){
-            if(md5(req.body.password)===foundUser.password){
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser=new User({
+            email:req.body.username,
+            password:hash 
+        });
+        newUser.save(function(err){
+            if(!err){
                 res.render("secrets");
             }
             else{
-                console.log("Username or password is incorrect")
-            }
-        }
-        else{
-            if(err){
                 console.log(err);
             }
+        })
+    });
+});
+
+app.post("/login",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    User.findOne({email:username},function(err,foundUser){
+        if(foundUser){
+            bcrypt.compare(password,foundUser.password,function(err,result){
+                if(result){
+                    res.render("secrets");
+                }
+            })
+        }
+        else{
+            console.log(err);
         }
     })
 })
